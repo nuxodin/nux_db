84dd3bac516dcceb89c6325212933e1abd1fdbcc
Tobias Buschor <tobias.buschor@shwups.ch>
2019-06-17 11:29:23 +0200

    Update Cell.js

--- Cell.js
+++ Cell.js
@@ -4,10 +4,11 @@ class Cell {
         this.row = row;
         this.name = name;
     }
+
     get value() {
         if (this.P_value === undefined) {
             this.P_value = this.row.table.rowIdToWhere(this.row.eid).then(where=>{
-                return this.row.table.db.one("SELECT "+this.name+" FROM "+this.row.table+" WHERE "+where+" "); // return not needed
+                return await this.row.table.db.one("SELECT "+this.name+" FROM "+this.row.table+" WHERE "+where+" "); // return not needed
             });
         }
         return this.P_value;
